6ed8287109260364eda2fc3033813f11dc665a45
罗华一 <luohy15@mails.tsinghua.edu.cn>
2018-05-10 22:53:56 +0800

    update alloc_proc()

--- labcodes/lab6/kern/process/proc.c
+++ labcodes/lab6/kern/process/proc.c
@@ -117,6 +117,12 @@ alloc_proc(void) {
         memset(proc->name, 0, PROC_NAME_LEN);//进程名
         proc->wait_state = 0;//初始化进程等待状态  
         proc->cptr = proc->optr = proc->yptr = NULL;//进程相关指针初始化  
+        proc->rq = NULL;
+        list_init(&(proc->run_link));
+        proc->time_slice = 0;
+        proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
+        proc->lab6_stride = 0;
+        proc->lab6_priority = 0;
     }
     return proc;
 }
